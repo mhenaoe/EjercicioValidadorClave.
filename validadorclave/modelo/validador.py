5f5d2b3f9140c1ab6801805fe27c646ec1a0d8dd
Mariana <mhenaoecheverri@gmail.com>
2025-05-12 12:17:40 -0500

    Class ReglaValidacion, def _validar_longitud, _contiene_mayuscula, _contiene_minuscula, _contiene_numero, es_valida added

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,21 @@
 from abc import ABC, abstractmethod
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada: int):
+        self._longitud_esperada = longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave: str) -> bool:
+        return any(c.isdigit() for c in clave)
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
